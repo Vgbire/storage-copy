ec982f42e6ed3864bbcb029f05d7028edee4a056
Vgbire <1053254202@qq.com>
2024-05-11 10:41:05 +0800

    feat: 删掉多余代码

--- extension/content/cookie.js
+++ extension/content/cookie.js
@@ -1,21 +1,3 @@
-function getCookie(cname) {
-  let name = cname + '='
-  let ca = document.cookie.split(';')
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i].trim()
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length)
-    }
-  }
-  return ''
-}
-
-function setCookie(cname, cvalue, exdays, domain) {
-  let day = new Date()
-  day.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
-  document.cookie = `${cname}=${cvalue};expires=${day.toGMTString()};path=/;domain=${domain}`
-}
-
 const xCookie = {
   get: function (sKey) {
     return (
